4a4b342e290381ef2ec91b1851a1628c23cffe25
HlebLegendarny <jauqhlshsajq@gmail.com>
2025-03-18 12:45:59 +0300

    )Init of libs and start of actual development)

---
+++ lib/tenorway.lua
@@ -0,0 +1,19 @@
+--              PLANS
+--  1. Написать всё в одном файле
+--  2. Добиться полной независимости 
+--   от других библиотек в будущем
+
+local TenorionEnigne = {
+    __NAME = "Tenorway";
+    __DESCRIPTION = "The simple and very local lib for manipulating and playing specified tenori-on-like patterns for future games OSTs";
+    __VERSION = "0.0.1";
+    __AUTHOR = "HlebLegendarny";
+    __LICENSE = [[None]];
+}
+local PatternExample = {
+    name = "example song",
+    author = "HlebLegendarny",
+    description = "manual example of pattern",
+    
+}
+return TenorionEnigne
