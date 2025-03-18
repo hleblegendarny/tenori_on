a6106b72716d5b6b22eb0bb1f7f19187358f2698
HlebLegendarny <jauqhlshsajq@gmail.com>
2025-03-18 22:11:32 +0300

    gooi test -- Exit button

--- main.lua
+++ main.lua
@@ -1,7 +1,3 @@
 local scenery = require "lib.common.scenery"
 scenery = scenery("tenori_on", "scenes")
-scenery:hook(love)
-
-function love.keypressed(k)
-    if k == "escape" then love.event.quit() end
-end
+scenery:hook(love, {'load', 'update', 'draw', 'mousepressed', 'mousereleased', 'keypressed'})
--- scenes/tenori_on.lua
+++ scenes/tenori_on.lua
@@ -1,5 +1,6 @@
-local class = require "lib.common.class"
-local TenoriOnScene = class:extend()
+local Class = require "lib.common.class"
+require "lib.visual.gooi.gooi"
+local TenoriOnScene = Class:extend()
 -- attributes
 TenoriOnScene.timer_limit = 30
 TenoriOnScene.timer = TenoriOnScene.timer_limit
@@ -11,9 +12,11 @@ TenoriOnScene.w, TenoriOnScene.h = love.graphics.getDimensions()
 --scene_methods
 
 function TenoriOnScene:load() 
-    
-    print(love) 
-
+    gooi.newButton({text = "exit",w=150,h=30,x=100,y=100}):onRelease(function ()
+        gooi.confirm({text = "are ya?", ok = function ()
+            love.event.quit()
+        end}) 
+    end):warning()
 end
 
 function TenoriOnScene:update(dt)
@@ -32,5 +35,19 @@ function TenoriOnScene:draw()
         self.h/2-30,                      --y
         40                                --limit
     )
+    gooi.draw()
 end
+
+function TenoriOnScene:mousepressed(x,y,id)
+    gooi.pressed(id,x,y)
+end
+function TenoriOnScene:mousereleased(x,y,id)
+    gooi.released(id,x,y)
+    print(TenoriOnScene)
+end
+function TenoriOnScene:keypressed(k)
+    if k == "escape" then gooi.confirm({text = "Gaddem!", ok = function () love.event.quit() end}) end
+end
+
+
 return TenoriOnScene
